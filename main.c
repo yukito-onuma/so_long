724781db97c57cdcc69b877a8dff9cee20cd7726
Yukito Onuma <yonuma@c5r4s2.42tokyo.jp>
2024-11-17 20:38:56 +0900

    yuo

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yonuma <yonuma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:46:56 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/14 19:06:51 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/17 19:37:41 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,43 @@
 #include <X11/keysym.h>
 #include "libmlx/mlx.h"
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+
 int event_handler(int key, void *mlx)
 {
 	(void)mlx;
 	if (key == XK_Escape)
 		exit(0);
-	printf("called\n");
 	return (0);
 }
 
-int main(void)
+int	key_hook(int keycode, t_vars *vars)
 {
- void *mlx;
- void *mlx_win;
+	printf("Hello from key_hook!\n");
+	return (0);
+}
 
- mlx = mlx_init();
- mlx_win = mlx_new_window(mlx, 300, 300, "Hello World");
- mlx_hook(mlx_win, KeyPress, KeyPressMask, event_handler, mlx);
- mlx_loop(mlx);
+int	main(void)
+{
+	t_vars	vars;
+	int i = 0;
+	char	map[2] = {1,0};
+	
+	while (i < 2)
+	{
+		i++;
+		if (map[i] == 1)
+			
+		else
+	}
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_hook(vars.win, KeyPress, KeyPressMask, event_handler, vars.mlx);
+	mlx_loop(vars.mlx);
 }
 
